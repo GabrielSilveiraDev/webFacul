59414ba6fee8d3c88e1f7852b74db73ebd697165
Gabriel Pessoal <gabrielfornaza@yahoo.com.br>
2022-11-26 14:42:52 -0300

    a

--- js/script.js
+++ js/script.js
@@ -29,12 +29,24 @@ function toggleButton(){
 
 }
 
+function enableSubmit(){
+    let inputs = document.getElementsByClassName('req');
+    let btn = document.querySelector('#botaoConta');
+    let isValid = true;
+ 
+    for (var i = 0; i < inputs.length; i++){
+      let changedInput = inputs[i];
+      if (changedInput.value.trim() === "" || changedInput.value === null){
+        isValid = false;
+        break;
+      }
+    }
+    btn.disabled = !isValid;
+  }
+
+
 function limparLogin(){
     document.getElementById("login-email").value = null;
     document.getElementById("login-password").value = null;
 }
 
-function limparConta(){
-    document.querySelector()
-
-}
